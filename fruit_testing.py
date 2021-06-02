8e501f8a7e42a7077dcc356f6f47fd0e30f2c4ff
Eleanor Berry <eleanor@Eleanors-MBP.smc.local>
2021-06-02 12:34:05 +1200

    Planning update

---
+++ fruit_testing.py
@@ -0,0 +1,22 @@
+def add_to_fruit_bowl():
+    return None
+
+def review_fruit(l):
+    for x in l:
+        output = "{:<10} -- {:>4}".format(x[0], x[1])
+        print(output)
+    return None
+
+def main():
+
+    fruit_list = [
+        ["apples", 2],
+        ["pears", 0],
+        ["quinces", 3],
+        ["lemons", 7]
+    ]
+
+    review_fruit(fruit_list)
+    return None
+
+main()
